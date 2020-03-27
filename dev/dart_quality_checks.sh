a239f5082dde6ddc8db63a4ea97aaa76f20c5b9e
Alexei Sintotski <alexei.sintotski@gmail.com>
2020-03-27 11:36:59 +0100

    The tool runs is being executed during quality checks

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-#dart example/main.dart
+dart bin/main.dart probe
 dartanalyzer --fatal-hints .
 pub run test
 pub run dependency_validator --ignore=functional_data_generator,sum_types_generator,test_coverage
