e0b34cce275ca8b806f10bceea2eae46773af34a
Alexei Sintotski <alexei.sintotski@gmail.com>
2020-06-07 09:57:50 +0200

    Better test coverage with incremental bootstrapping smoke test

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -2,6 +2,7 @@
 
 dart bin/main.dart probe --verbose
 dart bin/main.dart evolve --dry-run --exclude test/integration_test_sets/package_with_inconsistent_dep_spec --verbose
+dart bin/main.dart boot --mode=incremental
 dart bin/main.dart boot --mode=basic
 dart bin/main.dart boot --mode=incremental
 dartanalyzer --fatal-hints .
